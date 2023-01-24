4cf2a5da6bacbce4d64879a0d4a89aafb6c15cf3
tejashwar27 <tejashwar27@gmail.com>
2023-01-24 15:02:38 +0530

    Switched off highlights for search

--- lua/frostbitten-jello/options.lua
+++ lua/frostbitten-jello/options.lua
@@ -19,7 +19,7 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 
 -- Search Parameters
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
