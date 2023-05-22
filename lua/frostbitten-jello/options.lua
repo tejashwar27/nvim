8c57a9c1ca0eeeb8d72a9d54dc8e1c39b943c057
tejashwar27 <tejashwar27@gmail.com>
2023-05-22 13:16:55 +0530

    Search Highlight On

--- lua/frostbitten-jello/options.lua
+++ lua/frostbitten-jello/options.lua
@@ -20,7 +20,7 @@ vim.opt.wrap = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
