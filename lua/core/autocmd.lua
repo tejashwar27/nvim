ba9a3d9e393518f38cf256c4529d8c33d3ff6549
tejashwar27 <tejashwar27@gmail.com>
2023-06-13 19:29:18 +0530

    Added autocmd.lua

---
+++ lua/core/autocmd.lua
@@ -0,0 +1,33 @@
+-- Open help in a new buffer instead of a vsplit
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  pattern = '*',
+  callback = function(event)
+    if vim.bo[event.buf].filetype == 'help' then
+      vim.cmd.only()
+      vim.bo.buflisted = true
+    end
+  end,
+})
+
+-- Hide lualine when in command mode
+vim.api.nvim_create_autocmd('CmdlineEnter', {
+  pattern = '*',
+  callback = function()
+    require('lualine').hide({})
+  end,
+})
+
+-- Show lualine when command mode is exit
+vim.api.nvim_create_autocmd('CmdlineLeave', {
+  pattern = '*',
+  callback = function()
+    require('lualine').hide({ unhide = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd({'BufEnter', 'BufRead', 'BufNewFile'}, {
+  pattern = '*_repl',
+  callback = function()
+    vim.o.filetype = 'cpp'
+  end,
+})
