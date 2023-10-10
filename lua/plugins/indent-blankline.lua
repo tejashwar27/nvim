a2b65b0cd0127784f5151ac3eb40e2e9a4488ca0
tejashwar27 <tejashwar27@gmail.com>
2023-10-10 19:58:19 +0530

    changed syntax for latest version

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -6,14 +6,15 @@ return {
     --vim.opt.listchars:append 'space:⋅'
     vim.opt.listchars:append 'eol:↴'
 
-    require('indent_blankline').setup {
-      enabled = true,
-      colored_indent_levels = true,
-      char = '¦', -- ┊┆ ⎸
-      show_trailing_blankline_indent = false,
-      space_char_blankline = ' ',
-      show_current_context = true,
-      show_current_context_start = false,
-    }
+    require('ibl').setup()
+    -- require('ibl').setup() {
+    --   enabled = true,
+    --   colored_indent_levels = true,
+    --   char = '¦', -- ┊┆ ⎸
+    --   show_trailing_blankline_indent = false,
+    --   space_char_blankline = ' ',
+    --   show_current_context = true,
+    --   show_current_context_start = false,
+    -- }
   end,
 }
